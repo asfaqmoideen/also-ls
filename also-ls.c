c814821bf272d6fb0b5e8085588a9d30c7b2eb55
Asfaq Moideen <asfaqmoideen@gmail.com>
2026-06-12 14:29:42 +0000

    basic working ls implemented

--- also-ls.c
+++ also-ls.c
@@ -11,8 +11,13 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    struct dirent *entry ;
     
-    // printf("%s \n", path);
+    while((entry = readdir(dir)) != NULL){
+        if(entry->d_name[0] == '.') continue;
+        printf("%s\n", entry->d_name);
+    }
 
+    closedir(dir);
     return 0;
 }
